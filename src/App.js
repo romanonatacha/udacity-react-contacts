cf66953d4d3b104dc6271d27afda0de41d4c0820
natacha romano <romano.n@hotmail.com>
2018-11-17 10:55:49 -0200

    removing useless files and content

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'June' },
-          { name: 'Rory' },
-          { name: 'Lorelai' }
-        ]} />
-        <ContactList contacts={[
-          { name: 'Andy' },
-          { name: 'Luca' },
-          { name: 'Fred' }
-        ]}/>
-      </div>
-    );
-  }
-}
 
 export default App;
